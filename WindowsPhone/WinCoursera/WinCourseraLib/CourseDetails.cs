979f73144aae4d952fc9a65f58595af006fa9af5
ajaymartin <ajaymartin@gmail.com>
2012-12-17 22:30:12 -0800

    initial commit of windows phone 8 companion application to Coursera. https://www.coursera.org not an official release not for general usage.

---
+++ WindowsPhone/WinCoursera/WinCourseraLib/CourseDetails.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CourseraLib
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class CourseDetails
+    {
+        [DataMember]
+        public string id { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+
+        [DataMember]
+        public string large_icon { get; set; }
+
+        [DataMember]
+        public string short_description { get; set; }
+
+        [DataMember]
+        public string instructor { get; set; }
+    }
+}
